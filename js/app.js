2dd557814283cd8c16843a5dde580bdcb2657876
Efren Espinosa <Aztecjag@users.noreply.github.com>
2015-03-31 22:51:55 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
  * a position, an image, and a render function which
  * should not differ between different types (player, enemy, any pickups/blockers, etc.) 
  */ 
+var Resources;
 var CanvasItem = function(x,y,sprite) {
     this.x = x || 0;
     this.y = y || 0;
@@ -26,19 +27,19 @@ var CanvasItem = function(x,y,sprite) {
 var Enemy = function(x,y) {
     this.base = CanvasItem;
     this.base(x,y,'images/enemy-bug.png');
-    this.velocity = Math.floor(Math.random()*(121)+100);// Randomize enemy speed. This method found at http://stackoverflow.com/questions/4959975/generate-random-value-between-two-numbers-in-javascript 
+    this.velocity = Math.floor(Math.random()*(121)+100);// This makes enemy speed random. 
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if ( this.x <= ctx.canvas.width) {// let the Enemy appear to smoothly move past right side
+    if ( this.x <= ctx.canvas.width) {
         this.x += this.velocity*dt;
     } else {
-        this.x = -this.width;// Start enemy entirely beyond left side, then have it appear to move smoothly into canvas
+        this.x = -this.width;
     }
     if (checkCollision(player,this)) {
         player.reset();
@@ -50,18 +51,11 @@ Enemy.prototype.reset = function() {
     this.velocity = Math.floor(Math.random()*(121)+100);
 };
 
-/*
-* Draw the enemy on the screen, required method for game
-* NOTE: Moved render function into CanvasItem constructor
- 
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-*/
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var animate;
 var Player = function(x,y,xStep,yStep) {
     this.base = CanvasItem;
     this.base(x,y,'images/char-boy.png');
@@ -69,30 +63,30 @@ var Player = function(x,y,xStep,yStep) {
     this.xStep = xStep || 0;//Number of pixels to move per keystroke in both x and y directions.
     this.yStep = yStep || 0;
     
-    // Set up timers to track total time per round and best time.
+    // One timer will track the total time per round and the other timer will track the best time.
     this.startTime = new Date();
     this.timer = this.startTime;
     this.totalTime = null;
-    console.log("Player start time: " + this.timer);
+    console.log("Start time: " + this.timer);
 };
 
-Player.prototype.reset = function(checkTime) {//send player back to start, update best time if player has reached top, reset player timer
-    this.totalTime = this.timer - this.startTime;// time in milliseconds.
+Player.prototype.reset = function(checkTime) {// This sends the player back to the beginning and updates the best time when the player reaches the top. 
+    this.totalTime = this.timer - this.startTime;// It also resets the timer in milliseconds.
     this.startTime = new Date();
     this.timer = this.startTime;
     if (!checkTime) {
         this.x = this.xInit;
         this.y = this.yInit;
         pClock.reset();
-        console.log("Starting over");
+        console.log("Restarting");
     } else {
         if (bestTime === 0 || this.totalTime < bestTime) bestTime = this.totalTime;
         bstClock.drawClock(bestTime);
-        console.log("You win! Total time: " + formatTimeString(this.totalTime) + " - Best Time: " + formatTimeString(bestTime));    
+        console.log("You won! Total time: " + formatTimeString(this.totalTime) + " - Best Time: " + formatTimeString(bestTime));    
     }
     
 };
-Player.prototype.update = function(x,y) {
+Player.prototype.update = function() {
     /* this is called every time main is called in engine.js.
      * simply update the timer to the new system time.
      * When game ends, start time will be subtracted from this.timer value
@@ -141,7 +135,7 @@ GameClock.prototype.drawClock = function (ms) {// ms is number of milliseconds. 
 GameClock.prototype.reset = function () {
     this.startTime = new Date();
 };
-// End GameClock
+// Game clock ends
 
 
 // Now instantiate your objects.
@@ -149,6 +143,9 @@ GameClock.prototype.reset = function () {
 // Place the player object in a variable called player
 var allEnemies = [];
 var player;
+var ctx;
+var pClock;
+var bstClock;
 var bestTime = 0;// this is time in milliseconds
 function buildAll() {
     console.log("got to buildAll");
@@ -159,8 +156,8 @@ function buildAll() {
     }
     player = new Player(0,382,101,83);// 101 is width of all blocks, 83 is defined in engine.js as the default y step size when adding the blocks.
     
-    pClock = new GameClock(120,30);// Clock to display current player time
-    bstClock = new GameClock(340,30);// Clock to display best time
+    pClock = new GameClock(120,30);// This clock shows the current time.
+    bstClock = new GameClock(340,30);// This clock shows the best time.
     
     pClock.drawClock(0);
     bstClock.drawClock(0);
@@ -182,20 +179,21 @@ document.addEventListener('keyup', function(e) {
 });
 
 // Listen for clicks on "New Game" button, call "reset" in engine.js
+var reset;
 window.onload = function () {
-    document.getElementById("newGame").addEventListener("click", function(e) {
-        console.log("New Game clicked");
+    document.getElementById("newGame").addEventListener("click", function() {
+        console.log("New Game selected");
         reset();// game reset in engine.js
     },false);
 
 };
 
-// Add ability for user to change the player character sprite via radio button selection
+// Allows the user to change the player 
 var changeChar = function() {
-    console.log("Character selected: " + this.value);
+    console.log("Player selected: " + this.value);
     player.sprite="images/" + this.value;
     this.blur();
-    player.render();// redraw player sprite with new image
+    player.render();// redraws player
 };
 
 var radios = document.getElementsByName('gameChar');
@@ -203,14 +201,9 @@ var radios = document.getElementsByName('gameChar');
 for(var i = radios.length; i--; ) {
     radios[i].onclick = changeChar;
 }
-// End sprite change
+// Player change ends
 
 
-/*
-The following function adapted from an example found at
-http://stackoverflow.com/questions/20846944/check-if-two-items-overlap-on-a-canvas-using-javascript
-Note: we're subtracting 20px from left and right side of player to account for some transparency in player image
-*/
 function checkCollision(pl,en) {
     return !( en.x > (pl.x + pl.width - 20) || 
             (en.x + en.width) <  (pl.x + 20) || 
@@ -227,5 +220,3 @@ function formatTimeString(thisTime) {
     
     return (thisMins + ":" + thisSecs);
 }
-
-//dummy comment here
